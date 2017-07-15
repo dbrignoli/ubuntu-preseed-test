ef02cde21dd83c8b73d493f02a0784df84815331
Delio Brignoli <dbrignoli@audioscience.com>
2017-07-15 19:00:46 +0200

    tcs-connect: fix download URL and prevent script overwriting in case of error

--- tcs-connect/start.sh
+++ tcs-connect/start.sh
@@ -3,7 +3,7 @@
 THIS_SCRIPT_PATH=${0}
 
 md5sum ${THIS_SCRIPT_PATH} > ${THIS_SCRIPT_PATH}.md5
-wget -O ${THIS_SCRIPT_PATH} https://github.com/dbrignoli/ubuntu-preseed-test/tcs-connect/$(basename ${THIS_SCRIPT_PATH})
+wget -O ${THIS_SCRIPT_PATH}.new https://raw.githubusercontent.com/dbrignoli/ubuntu-preseed-test/master/tcs-connect/start.sh && mv -f ${THIS_SCRIPT_PATH}.new ${THIS_SCRIPT_PATH}
 # if the updated script does not match the running one, restart
 md5sum -c ${THIS_SCRIPT_PATH}.md5 || exec ${THIS_SCRIPT_PATH}
 chmod a+x ${THIS_SCRIPT_PATH}
