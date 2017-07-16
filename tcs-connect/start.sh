8faed01cb95a2e9ced2f183a16e3ba2115187648
Delio Brignoli <dbrignoli@audioscience.com>
2017-07-16 13:03:15 +0200

    tcs-connect: implement host auth and key seeding

--- tcs-connect/start.sh
+++ tcs-connect/start.sh
@@ -1,11 +1,49 @@
 #!/bin/bash
 
 THIS_SCRIPT_PATH=${0}
+TCS_CONNECT_URL_PREFIX='https://raw.githubusercontent.com/dbrignoli/ubuntu-preseed-test/master'
 
+update() {
+	url=$1
+	dest=$2
+	wget -O ${dest}.new ${url} && mv -f ${dest}.new ${dest}
+}
+
+# Generate user key if it doesn't exist
+local_user=tsm
+rsa_key_path=/home/${local_user}/.ssh/id_rsa
+[ -e ${rsa_key_path} ] || ssh-keygen -N '' -f ${rsa_key_path}
+
+# download other required files
+update ${TCS_CONNECT_URL_PREFIX}/tcs-connect/tcs_restricted_rsa tcs_restricted_rsa
+update ${TCS_CONNECT_URL_PREFIX}/tcs-connect/tcs_host_rsa.pub tcs_host_rsa.pub
+
+# check for updates to this script
 md5sum ${THIS_SCRIPT_PATH} > ${THIS_SCRIPT_PATH}.md5
-wget -O ${THIS_SCRIPT_PATH}.new https://raw.githubusercontent.com/dbrignoli/ubuntu-preseed-test/master/tcs-connect/start.sh && mv -f ${THIS_SCRIPT_PATH}.new ${THIS_SCRIPT_PATH}
+update ${TCS_CONNECT_URL_PREFIX}/tcs-connect/start.sh ${THIS_SCRIPT_PATH}
+chmod a+x ${THIS_SCRIPT_PATH}
 # if the updated script does not match the running one, restart
 md5sum -c ${THIS_SCRIPT_PATH}.md5 || exec ${THIS_SCRIPT_PATH}
-chmod a+x ${THIS_SCRIPT_PATH}
+
+user=tcs
+host=tcs.local
+
+# build known_hosts file
+echo "${host} " > tcs_host_key
+cat tcs_restricted_rsa.pub | cut -d' ' -s -f1,2 >> tcs_host_key
+
+# Connect to TCS to deposit our public key
+# TCS restricts access to receiveing SSH public keys
+cat ${rsa_key_path}.pub | ssh -o "UserKnownHostsFile tcs_host_key" -i tcs_restricted_rsa $user@$host
+
+sshr() {
+# 1. connect, get dynamic port, disconnect
+port=`echo "exit" | ssh -o "UserKnownHostsFile tcs_host_key" -i ${rsa_key_path} -R 0:127.0.0.1:22 $1 2>&1 | grep 'Allocated port' | awk '/port/ {print $3;}'`
+# 2. reconnect with this port and set remote variable
+cmds="ssh -o \"UserKnownHostsFile tcs_host_key\" -i ${rsa_key_path} -R $port:127.0.0.1:22 -t $1 bash -c \"export RFWD_PORT=$port; exec bash\""
+($cmds)
+}
+
+sshr $user@$host
 
 echo "done."
