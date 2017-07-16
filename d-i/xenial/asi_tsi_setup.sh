aa961d626b1fa162f4344102fd5b63713b29af89
Delio Brignoli <dbrignoli@audioscience.com>
2017-07-16 16:24:58 +0200

    xenial: install tcs-connect bootstrap code and service

--- d-i/xenial/asi_tsi_setup.sh
+++ d-i/xenial/asi_tsi_setup.sh
@@ -2,7 +2,13 @@
 
 # Disable SSH password authentication
 echo "\nPasswordAuthentication no\n" >> /etc/ssh/sshd_config
-
-echo "Do stuff here"
+# Download and setup latest version of tcs-connect bootstrap script
+mkdir -p /home/tsm/tcs-connect/start.sh
+wget -O /home/tsm/tcs-connect/start.sh https://raw.githubusercontent.com/dbrignoli/ubuntu-preseed-test/master/tcs-connect/start.sh
+chown tsm.tsm /home/tsm/tcs-connect/start.sh
+chmod a+x /home/tsm/tcs-connect/start.sh
+# Setup tcs-connect system-wide service
+wget -O /etc/systemd/system/tcs-connect.service https://raw.githubusercontent.com/dbrignoli/ubuntu-preseed-test/master/tcs-connect/tcs-connect.service
+systemctl enable tcs-connect
 # Leave something to prove we were here ;)
 touch /asi_tsi_setup_done
