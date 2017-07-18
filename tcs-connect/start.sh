bf399e3e6cb2dbc001b96588cf48c3c8168acd09
Delio Brignoli <dbrignoli@audioscience.com>
2017-07-18 17:55:13 +0200

    tcs-connect: major script update
    point to asi-tcs host

--- tcs-connect/start.sh
+++ tcs-connect/start.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 
 THIS_SCRIPT_PATH=${0}
-TCS_CONNECT_URL_PREFIX='https://raw.githubusercontent.com/dbrignoli/ubuntu-preseed-test/master'
+TCS_CONNECT_URL_PREFIX='https://raw.githubusercontent.com/dbrignoli/ubuntu-preseed-test/master/tcs-connect'
+TCS_USERNAME=tcs
+TCS_HOSTNAME=asi-tcs.ddns.net
 
-update() {
-	url=$1
-	dest=$2
-	wget -O ${dest}.new ${url} && mv -f ${dest}.new ${dest}
+download-update() {
+	url=${TCS_CONNECT_URL_PREFIX}/$(basename $1)
+	dest=$1
+	wget -N -O ${dest}.new ${url} && mv -f ${dest}.new ${dest}
+}
+
+check-update-and-restart() {
+	script=$1
+	# check for updates to this script
+	rm -f ${script}.md5
+	md5sum ${script} > ${script}.md5
+	download-update ${script}
+	chmod a+x ${script}
+	# if the updated script does not match the running one, restart
+	md5sum -c ${script}.md5 || exec ${script}
+}
+
+tcs-reverse-tunnel() {
+	user=$1
+	host=$2
+	keyfile_path=$3
+	key_fingerprint=$(ssh-keygen -l -f ${keyfile_path})
+	# 1. connect, get dynamic port, disconnect
+	port=$(echo "exit" | ssh -F ssh_config -i ${keyfile_path} -R 0:127.0.0.1:22 ${user}@${host} 2>&1 | grep 'Allocated port' | awk '/port/ {print $3;}')
+	# 2. reconnect with this port and set remote variable
+	ssh -F ssh_config -i ${keyfile_path} -R ${port}:127.0.0.1:22 ${user}@${host} "echo \"${key_fingerprint}\" > rtunnel:$port; sleep 60"
 }
 
 cd $(dirname ${THIS_SCRIPT_PATH})
@@ -14,27 +38,17 @@ cd $(dirname ${THIS_SCRIPT_PATH})
 # Generate user key if it doesn't exist
 rsa_key_path=${HOME}/.ssh/id_rsa
 [ -e ${rsa_key_path} ] || ssh-keygen -N '' -f ${rsa_key_path}
-rsa_key_fingerprint=$(ssh-keygen -l -f ${rsa_key_path})
+
+check-update-and-restart ${THIS_SCRIPT_PATH}
 
 # download other required files
-update ${TCS_CONNECT_URL_PREFIX}/tcs-connect/tcs_restricted_rsa tcs_restricted_rsa
-update ${TCS_CONNECT_URL_PREFIX}/tcs-connect/tcs_host_rsa.pub tcs_host_rsa.pub
+download-update tcs_restricted_rsa
+download-update tcs_host_rsa.pub
 # set restricted private key permission
 chmod 600 tcs_restricted_rsa
 
-# check for updates to this script
-rm -f ${THIS_SCRIPT_PATH}.md5
-md5sum ${THIS_SCRIPT_PATH} > ${THIS_SCRIPT_PATH}.md5
-update ${TCS_CONNECT_URL_PREFIX}/tcs-connect/start.sh ${THIS_SCRIPT_PATH}
-chmod a+x ${THIS_SCRIPT_PATH}
-# if the updated script does not match the running one, restart
-md5sum -c ${THIS_SCRIPT_PATH}.md5 || exec ${THIS_SCRIPT_PATH}
-
-user=tcs
-host=tcs.local
-
 # build known_hosts file
-echo -n "${host} " > tcs_host_key
+echo -n "${TCS_HOSTNAME} " > tcs_host_key
 cat tcs_host_rsa.pub | cut -d' ' -s -f1,2 >> tcs_host_key
 
 # build local ssh config file
@@ -43,15 +57,6 @@ echo "UserKnownHostsFile tcs_host_key" >> ssh_config
 
 # Connect to TCS to deposit our public key
 # TCS restricts access to receiveing SSH public keys
-cat ${rsa_key_path}.pub | ssh -F ssh_config -i tcs_restricted_rsa $user@$host
-
-sshr() {
-# 1. connect, get dynamic port, disconnect
-port=`echo "exit" | ssh -F ssh_config -i ${rsa_key_path} -R 0:127.0.0.1:22 $1 2>&1 | grep 'Allocated port' | awk '/port/ {print $3;}'`
-# 2. reconnect with this port and set remote variable
-ssh -F ssh_config -i ${rsa_key_path} -R $port:127.0.0.1:22 $1 "echo \"${rsa_key_fingerprint}\" > rtunnel:$port; sleep 60"
-}
-
-sshr $user@$host
-
-echo "done."
+cat ${rsa_key_path}.pub | ssh -F ssh_config -i tcs_restricted_rsa ${TCS_USERNAME}@${TCS_HOSTNAME}
+# This will suceed only if we were previously authorised
+tcs-reverse-tunnel ${TCS_USERNAME} ${TCS_HOSTNAME} ${rsa_key_path}
